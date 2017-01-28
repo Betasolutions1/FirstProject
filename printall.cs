662ab9d5bb345b8c2ed8eab20c28773bb26bad54
BetaSolutions1 <beta.solutions@mail.com>
2017-01-28 16:26:37 +0530

    Added new files

---
+++ printall.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace IFRPCVC
+{
+    public partial class printall : Form
+    {
+        public printall()
+        {
+            InitializeComponent();
+        }
+
+        private void printall_Load(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection con = new SqlConnection(CDatabase.ConnectionStr);
+        public void FillData()
+        {
+            listView2.Items.Clear();
+            con.Close();
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("select DP_ID from TBL_DUTY_PASS ", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read() == true)
+            {
+                //listView2.CheckBoxes = true;
+                // Select the item and subitems when selection is made.
+                //listView2.FullRowSelect = true;
+                // Display grid lines.
+                //listView2.GridLines = true;
+                ListViewItem listItem = new ListViewItem();
+                listItem.Text = dr[0].ToString();
+               // listItem.SubItems.Add(dr[0].ToString());
+                listView2.Items.Add(listItem);
+
+            }
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillData();
+        }
+
+        private void listView2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            //if (listView2.SelectedItems.Count > 0)
+            //{
+            //    if (listView2.SelectedItems[0].Text != null)
+            //    {
+
+            //        label1.Text = listView2.SelectedItems[0].Text.ToString();
+            //    }
+            //    else if (label1.Text == "")
+            //    {
+            //        MessageBox.Show("Please select one ID");
+            //    }
+            //}
+            //if (listView2.CheckedItems.Count > 0)
+            //{
+            //    label1.Text = listView2.CheckedItems.ToString();
+            //}
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems.Count > 0)
+            {
+                if (listView2.SelectedItems[0].Text != null)
+                {
+
+                    label3.Text = listView2.SelectedItems[0].Text.ToString();
+                }
+                else if (label3.Text == "")
+                {
+                    MessageBox.Show("Please select one ID");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems.Count > 0)
+            {
+                if (listView2.SelectedItems[0].Text != null)
+                {
+
+                    label1.Text = listView2.SelectedItems[0].Text.ToString();
+                }
+                else if (label1.Text == "")
+                {
+                    MessageBox.Show("Please select one ID");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems.Count > 0)
+            {
+                if (listView2.SelectedItems[0].Text != null)
+                {
+
+                    label2.Text = listView2.SelectedItems[0].Text.ToString();
+                }
+                else if (label2.Text == "")
+                {
+                    MessageBox.Show("Please select one ID");
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems.Count > 0)
+            {
+                if (listView2.SelectedItems[0].Text != null)
+                {
+
+                    label4.Text = listView2.SelectedItems[0].Text.ToString();
+                }
+                else if (label4.Text == "")
+                {
+                    MessageBox.Show("Please select one ID");
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            prnt_vw f = new prnt_vw();
+            //string s = txtid.Text;
+            //f.txtprint = s;
+
+           // f.PrintReport(label1.Text, label2.Text, label3.Text, label4.Text);
+           // f.Load(label1.Text, label2.Text, label3.Text, label4.Text);
+            string a = label1.Text;
+            string b = label2.Text;
+            string c = label3.Text;
+            string d = label4.Text;
+            f.txtprint = a;
+            f.txtprint1 = b;
+            f.txtprint2 = c;
+            f.txtprint3 = d;
+            f.Owner = this;
+            f.Show();
+            //f.Dispose();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ prnt_vw.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace IFRPCVC
+{
+    public partial class prnt_vw : Form
+    {
+        public prnt_vw()
+        {
+            InitializeComponent();
+        }
+        CDatabase cd = new CDatabase();
+        public string txtprint
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+        public string txtprint1
+        {
+            get { return textBox2.Text; }
+            set { textBox2.Text = value; }
+        }
+        public string txtprint2
+        {
+            get { return textBox3.Text; }
+            set { textBox3.Text = value; }
+        }
+        public string txtprint3
+        {
+            get { return textBox4.Text; }
+            set { textBox4.Text = value; }
+        }
+        public void PrintReport(string Id,string Id1,string Id2,string Id3)
+        {
+            //prnt_all un = new prnt_all();
+            //SqlConnection cn = new SqlConnection(CDatabase.ConnectionStr);
+            //cn.Open();
+            //SqlCommand cmd = new SqlCommand("USP_GET_ID", cn);
+            //SqlCommand cmd1 = new SqlCommand("USP_GET_ID1", cn);
+            //SqlCommand cmd2 = new SqlCommand("USP_GET_ID2", cn);
+            //SqlCommand cmd3 = new SqlCommand("USP_GET_ID3", cn);
+
+            //cmd.CommandType = CommandType.StoredProcedure;
+
+            //cmd.Parameters.AddWithValue("@P_ID", Id);
+            //cmd1.CommandType = CommandType.StoredProcedure;
+            //cmd1.Parameters.AddWithValue("@P_ID1", Id1);
+            //cmd2.CommandType = CommandType.StoredProcedure;
+            //cmd2.Parameters.AddWithValue("@P_ID2", Id2);
+            //cmd3.CommandType = CommandType.StoredProcedure;
+            //cmd3.Parameters.AddWithValue("@P_ID3", Id3);
+            //SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //un.SetDataSource(dt);
+            //crystalReportViewer1.ReportSource = un;
+            //crystalReportViewer1.Refresh();
+            ////un.PrintOptions.PrinterName = "HP LaserJet 1020 (Copy 2)";
+            ////un.PrintOptions.PrinterName = "HP LaserJet 1020";
+            ////un.PrintOptions.PrinterName = "Microsoft Print to PDF";
+            ////un.PrintToPrinter(1, false, 1, 6);
+            //cn.Close();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = txtprint;
+            textBox2.Text = txtprint1;
+            textBox3.Text = txtprint2;
+            textBox4.Text = txtprint3;
+
+            prnt_all un = new prnt_all();
+            SqlConnection cn = new SqlConnection(CDatabase.ConnectionStr);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("USP_GET_ID", cn);
+            SqlCommand cmd1 = new SqlCommand("USP_GET_ID1", cn);
+            SqlCommand cmd2 = new SqlCommand("USP_GET_ID2", cn);
+            SqlCommand cmd3 = new SqlCommand("USP_GET_ID3", cn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@P_ID", textBox1.Text);
+            cmd1.CommandType = CommandType.StoredProcedure;
+            cmd1.Parameters.AddWithValue("@P_ID1", textBox2.Text);
+            cmd2.CommandType = CommandType.StoredProcedure;
+            cmd2.Parameters.AddWithValue("@P_ID2", textBox3.Text);
+            cmd3.CommandType = CommandType.StoredProcedure;
+            cmd3.Parameters.AddWithValue("@P_ID3", textBox4.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            un.SetDataSource(dt);
+
+            crystalReportViewer1.ReportSource = un;
+            crystalReportViewer1.Refresh();
+            //un.PrintOptions.PrinterName = "HP LaserJet 1020 (Copy 2)";
+            //un.PrintOptions.PrinterName = "HP LaserJet 1020";
+            // un.PrintOptions.PrinterName = "Microsoft Print to PDF";
+            // un.PrintToPrinter(1, false, 1, 6);
+            cn.Close();
+        }
+        //private void crystalReportViewer1_Load(object sender, EventArgs e)
+        //{
+        //    Untitled un = new Untitled();
+        //    SqlConnection cn = new SqlConnection(CDatabase.ConnectionStr);
+        //    cn.Open();
+        //    SqlCommand cmd = new SqlCommand("USP_GET_ID", cn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
+        //    cmd.Parameters.AddWithValue("@P_ID", textBox1.Text);
+        //    cmd.Parameters.AddWithValue("@P_ID1", textBox2.Text);
+        //    cmd.Parameters.AddWithValue("@P_ID2", textBox3.Text);
+        //    cmd.Parameters.AddWithValue("@P_ID3", textBox4.Text);
+        //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+        //    DataTable dt = new DataTable();
+        //    da.Fill(dt);
+        //    un.SetDataSource(dt);
+        //    crystalReportViewer1.ReportSource = un;
+        //    crystalReportViewer1.Refresh();
+        //    un.PrintOptions.PrinterName = "HP LaserJet 1020 (Copy 2)";
+        //    un.PrintOptions.PrinterName = "HP LaserJet 1020";
+        //    un.PrintOptions.PrinterName = "Microsoft Print to PDF";
+        //    un.PrintToPrinter(1, false, 1, 6);
+        //    cn.Close();
+        //}
+    }
+}
