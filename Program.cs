1a9f6f94e9c16b7b36b02f807e0128d3859d839f
BetaSolutions1 <beta.solutions@mail.com>
2017-01-28 16:46:27 +0530

    Modified

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace IFRPCVC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1876());
         }
     }
 }
